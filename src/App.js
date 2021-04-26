46a92e72d25400842f9470cd57e313cdfb957687
Chris Philbin <chris@chrisphilbin.net>
2021-04-26 16:26:28 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import {ExcelRenderer, OutTable} from 'react-excel-renderer'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import Input from '@material-ui/core/Input'
 
-function App() {
+const App = () => {
+
+  let [file, setFile]           = useState(null)
+  let [cols, setCols]           = useState(null)
+  let [rows, setRows]           = useState(null)
+  let [addresses, setAddresses] = useState([])
+
+  const handleConvert = () => {
+    ExcelRenderer(file, (err, resp) => {
+      if(err){
+        console.log(err)
+      } else {
+        setCols(resp.cols)
+        setRows(resp.rows)
+      }
+    })
+  }
+
+  const extract = () => {
+    rows.slice(1).map((row) => {
+      let a = row[2].match(/\(([^)]+)\)/)[1]
+      setAddresses(addresses => [...addresses, a])
+    })
+  }
+
+  console.log(rows, "rows")
+  console.log(addresses, "addresses")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Typography variant="h4">Step 1: Upload file</Typography>
+      <Input type="file" onChange={(e) => setFile(e.target.files[0]) } />
+      { file ? <Button onClick={() => handleConvert()}>Convert</Button> : null}
+      <Button onClick={() => extract()}>Extract</Button>
+    </Container>
+
+  )
 }
 
-export default App;
+export default App
