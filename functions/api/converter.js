8cdbc58263364914ba5406b0dcab7a757c6123d1
Chris Philbin <chris@chrisphilbin.net>
2021-05-07 07:08:06 -0400

    Cleaning up code and adding error handling

--- functions/api/converter.js
+++ functions/api/converter.js
@@ -4,16 +4,14 @@ const fetch = require("node-fetch")
 
 
 exports.getVerifiedAddress = (request, response) => {
-    //function takes in an address and verifies it against google's geocoding database
-    console.log(request.body, "<=== request body address")
     const address = request.body.address
-    console.log(address, "<=== address")
     const formattedUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_G_API}`
-    console.log(formattedUrl, "<=== formatted URL")
-    
     fetch(formattedUrl)
     .then(response => response.json())
     .then(data => {
         return response.json(data)
+    })
+    .catch(error => {
+        return response.status(400).json(error)
     })
 }
